c3eac9fc3623ba08801269b68fe3cc8dd0d798e8
fernando <jfernando.medinas@alumno.udg.mx>
2022-02-16 11:05:51 -0600

    Aun corss

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-cors = CORS(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 def distanciaPuntos(puntos, pesos, ubicaciones):
